2f6c79a7abf848a124dbd88f4a727208696f8f6b
Your Name <you@example.com>
2026-06-30 16:34:03 +0300

    rss html & css

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { setupCounter } from './counter.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
